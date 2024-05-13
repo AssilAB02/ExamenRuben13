df22fd75f65357eed5f3c9de8381c8dfa82e7a70
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-13 15:17:51 +0200

    FactoryMethod.java

--- src/main/java/Entregas/Entrega1/FactoryMethod.java
+++ src/main/java/Entregas/Entrega1/FactoryMethod.java
@@ -1,4 +1,17 @@
 package Entregas.Entrega1;
 
 public class FactoryMethod {
+    public static void main(String[] args) {
+        MediaCreator bookCreator = new BookCreator();
+        Media book = bookCreator.createMedia();
+        book.displayInfo();
+
+        MediaCreator magazineCreator = new MagazineCreator();
+        Media magazine = magazineCreator.createMedia();
+        magazine.displayInfo();
+
+        MediaCreator newspaperCreator = new NewspaperCreator();
+        Media newspaper = newspaperCreator.createMedia();
+        newspaper.displayInfo();
+    }
 }
