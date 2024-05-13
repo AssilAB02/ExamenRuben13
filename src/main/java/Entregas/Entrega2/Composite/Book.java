ea43520c1659e884251d7307e530083e45dfbbc0
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-13 15:44:05 +0200

    Book.java Entrega2

--- src/main/java/Entregas/Entrega2/Composite/Book.java
+++ src/main/java/Entregas/Entrega2/Composite/Book.java
@@ -1,4 +1,14 @@
 package Entregas.Entrega2.Composite;
 
-public class Book {
+class Book implements Media {
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("This is a book: " + title);
+    }
 }
