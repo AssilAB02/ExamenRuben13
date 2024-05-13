1543785d41b5a19c79dbda92fbaff75e0b630a1e
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-13 15:21:07 +0200

    Implementacion Media.java

--- src/main/java/Entregas/Entrega2/Media.java
+++ src/main/java/Entregas/Entrega2/Media.java
@@ -1,4 +1,23 @@
 package Entregas.Entrega2;
 
-public class Media {
+class Media {
+    private String title;
+    private String author;
+    private int year;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title + ", Author: " + author + ", Year: " + year);
+    }
 }
