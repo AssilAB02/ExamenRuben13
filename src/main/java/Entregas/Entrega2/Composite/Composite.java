238a8d28f1621b0e40e144100c1744925dbf0b93
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-13 15:48:07 +0200

    Composite.java.java Entrega2

--- src/main/java/Entregas/Entrega2/Composite/Composite.java
+++ src/main/java/Entregas/Entrega2/Composite/Composite.java
@@ -1,4 +1,17 @@
 package Entregas.Entrega2.Composite;
 
 public class Composite {
+    public static void main(String[] args) {
+        Media book1 = new Book("Book 1");
+        Media book2 = new Book("Book 2");
+        Media magazine1 = new Magazine("Magazine 1");
+
+        MediaGroup collection = new MediaGroup();
+        collection.add(book1);
+        collection.add(book2);
+        collection.add(magazine1);
+
+        collection.displayInfo();
+    }
 }
+
