9010737757225703fedc59ff063435ecae72ac82
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-13 15:46:59 +0200

    Magazine.java Entrega2

--- src/main/java/Entregas/Entrega2/Composite/Magazine.java
+++ src/main/java/Entregas/Entrega2/Composite/Magazine.java
@@ -1,4 +1,14 @@
 package Entregas.Entrega2.Composite;
 
-public class Magazine {
+class Magazine implements Media {
+    private String title;
+
+    public Magazine(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("This is a magazine: " + title);
+    }
 }
