8c7ffeb7054bdaa5a71e6729ab1fb252c54955f9
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-13 16:06:14 +0200

    Adapter.java Adapter Entrega 2

--- src/main/java/Entregas/Entrega2/Adapter/Adapter.java
+++ src/main/java/Entregas/Entrega2/Adapter/Adapter.java
@@ -1,4 +1,9 @@
 package Entregas.Entrega2.Adapter;
 
 public class Adapter {
+    public static void main(String[] args) {
+        File file = new File("book.txt");
+        Media book = new MediaAdapter(file);
+        book.displayInfo();
+    }
 }
