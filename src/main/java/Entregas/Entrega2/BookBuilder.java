cde7c3b47cde31af353d2693269ad05af00efc37
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-13 15:22:40 +0200

    Implementacion BookBuilder.java

--- src/main/java/Entregas/Entrega2/BookBuilder.java
+++ src/main/java/Entregas/Entrega2/BookBuilder.java
@@ -1,4 +1,28 @@
 package Entregas.Entrega2;
 
-public class BookBuilder {
+class BookBuilder implements MediaBuilder {
+    private Media book = new Media();
+
+    @Override
+    public MediaBuilder setTitle(String title) {
+        book.setTitle(title);
+        return this;
+    }
+
+    @Override
+    public MediaBuilder setAuthor(String author) {
+        book.setAuthor(author);
+        return this;
+    }
+
+    @Override
+    public MediaBuilder setYear(int year) {
+        book.setYear(year);
+        return this;
+    }
+
+    @Override
+    public Media build() {
+        return book;
+    }
 }
