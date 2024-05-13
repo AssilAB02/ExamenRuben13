a8f00c5bee168cde7f263954b4751611566df94b
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-13 15:36:34 +0200

    Builder.java

--- src/main/java/Entregas/Entrega1/Builder/Builder.java
+++ src/main/java/Entregas/Entrega1/Builder/Builder.java
@@ -1,4 +1,10 @@
 package Entregas.Entrega1.Builder;
 
 public class Builder {
+    public static void main(String[] args) {
+        MediaBuilder bookBuilder = new BookBuilder();
+        MediaDirector director = new MediaDirector();
+        Media book = director.createMedia(bookBuilder, "Book Title", "Author Name", 2024);
+        book.displayInfo();
+    }
 }
